7ea1d5fe7f70ce9b303b3eab94296302213c7ef4
roamgom <roamgom@gmail.com>
2019-05-26 15:28:30 +0900

    add flask cors

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from manuscript_cpu import inference_one
 
 app = Flask(__name__)
+
+cors = CORS(app, resources={
+    r"/*": {"origin": "*"},
+})
 
 
 @app.route('/', methods=['GET'])
